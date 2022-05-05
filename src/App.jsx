7d92f40999325cfd066baf26eac64b59711e9675
Mohamed Ismail <ismail@valorpaytech.com>
2022-05-05 18:17:43 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   )
 }
